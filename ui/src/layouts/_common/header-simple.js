b62f63cc614100075b5cdd02fa141a689cff1bd4
RoshanKolhe <roshan.kolhe@wolfizer.com>
2025-03-28 17:59:53 +0530

    help header changed

--- ui/src/layouts/_common/header-simple.js
+++ ui/src/layouts/_common/header-simple.js
@@ -6,14 +6,14 @@ import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 // theme
 import { bgBlur } from 'src/theme/css';
-// routes
-import { paths } from 'src/routes/paths';
 // hooks
 import { useOffSetTop } from 'src/hooks/use-off-set-top';
 // components
 import Logo from 'src/components/logo';
 import { RouterLink } from 'src/routes/components';
 //
+import { useState } from 'react';
+import { Box, Button, Popover, Typography } from '@mui/material';
 import { HEADER } from '../config-layout';
 import HeaderShadow from './header-shadow';
 import SettingsButton from './settings-button';
@@ -22,6 +22,19 @@ import SettingsButton from './settings-button';
 
 export default function HeaderSimple() {
   const theme = useTheme();
+
+  const [anchorEl, setAnchorEl] = useState(null);
+
+  const handleOpen = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const open = Boolean(anchorEl);
+  const id = open ? 'help-popup' : undefined;
 
   const offsetTop = useOffSetTop(HEADER.H_DESKTOP);
 
@@ -54,13 +67,79 @@ export default function HeaderSimple() {
           <SettingsButton />
 
           <Link
-            href='#'
+            href="#"
+            onClick={handleOpen}
             component={RouterLink}
             color="inherit"
             sx={{ typography: 'subtitle2' }}
           >
             Need help?
           </Link>
+
+          <Popover
+            id={id}
+            open={open}
+            anchorEl={anchorEl}
+            onClose={handleClose}
+            anchorOrigin={{
+              vertical: 'bottom',
+              horizontal: 'left',
+            }}
+            transformOrigin={{
+              vertical: 'top',
+              horizontal: 'left',
+            }}
+          >
+            <Box sx={{ p: 2, width: 250 }}>
+              <Box sx={{ display: 'flex', justifyContent: 'center', mb: 2 }}>
+                <Box
+                  component="img"
+                  src="/assets/icons/navbar/ic_chat.svg"
+                  sx={{ width: 80, height: 80 }}
+                />
+              </Box>
+              {/* Title and Subtitle */}
+              <Typography variant="h6" align="center" sx={{ mb: 1 }}>
+                Get in touch
+              </Typography>
+              <Typography variant="body2" align="center" color="text.secondary" sx={{ mb: 2 }}>
+                Reach Out for Assistance Anytime!
+              </Typography>
+              {/* Buttons */}
+              <Box sx={{ display: 'flex', justifyContent: 'space-around', mt: 2 }}>
+                <Button
+                  variant="contained"
+                  sx={{
+                    backgroundColor: 'black',
+                    color: 'white',
+                    px: 4, // Increased horizontal padding
+                    '&:hover': {
+                      backgroundColor: 'grey.800',
+                    },
+                  }}
+                  onClick={() => window.open('tel:+1234567890', '_self')}
+                >
+                  Call
+                </Button>
+                {/* Mail Button */}
+                <Button
+                  variant="contained"
+                  sx={{
+                    backgroundColor: 'white',
+                    color: 'black',
+                    px: 4, // Increased horizontal padding
+                    border: '1px solid black',
+                    '&:hover': {
+                      backgroundColor: 'grey.100',
+                    },
+                  }}
+                  onClick={() => window.open('mailto:help@example.com', '_self')}
+                >
+                  Mail
+                </Button>
+              </Box>
+            </Box>
+          </Popover>
         </Stack>
       </Toolbar>
 
