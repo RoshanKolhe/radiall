eb720dfea3b6e7830d6f9b2d7b31111b856255b9
RoshanKolhe <kolheroshan508@gmail.com>
2025-04-28 10:41:08 +0530

    text change

--- ui/src/sections/revisionHistory/view/revisionHistory-view.js
+++ ui/src/sections/revisionHistory/view/revisionHistory-view.js
@@ -25,7 +25,7 @@ export default function RevisionHistoryView() {
   return (
     <Container maxWidth={settings.themeStretch ? false : 'lg'}>
       <CustomBreadcrumbs
-        heading="Edit"
+        heading="View"
         links={[
           {
             name: 'Dashboard',
